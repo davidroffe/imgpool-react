0b8bc69d7a225b342c0b38055db3bf77b7c3970d
David Roffe <davidroffe@gmail.com>
2020-09-06 11:29:49 -0400

    Switching to Koa.js for server

--- server.js
+++ server.js
@@ -1,50 +1,26 @@
-const fsPromises = require('fs').promises;
 const fs = require('fs');
-const path = require('path');
-const http = require('http');
+const Koa = require('koa');
+const send = require('koa-send');
+const logger = require('koa-logger');
 require('dotenv').config();
+const app = new Koa();
+const publicPath = __dirname + '/public';
 
-const host = 'localhost';
-const port = process.env.PORT || 80;
-const requestListener = function(req, res) {
-  const fullPath =
-    __dirname + '/public' + (req.url === '/' ? '/index.html' : req.url);
-  console.log(
-    'request url: ' + req.url,
-    'file type: ' + path.extname(fullPath)
-  );
-
-  if (fs.existsSync(fullPath)) {
-    fsPromises
-      .readFile(fullPath)
-      .then((contents) => {
-        let contentType;
-
-        switch (path.extname(fullPath)) {
-          case '.html':
-            contentType = 'text/html';
-            break;
-          case '.js':
-            contentType = 'application/javascript';
-            break;
-          default:
-            break;
-        }
-
-        res.setHeader('Content-Type', contentType);
-        res.setHeader('Cache-Control', 'public');
-        res.writeHead(200);
-        res.end(contents);
-      })
-      .catch((err) => {
-        res.writeHead(500);
-        res.end(err);
-        return;
-      });
+app.use(logger());
+app.use(async (ctx, next) => {
+  await next();
+  if (ctx.status === 404) {
+    ctx.set('Cache-Control', 'public');
+    await send(ctx, '/index.html', { root: publicPath });
   }
-};
-
-const server = http.createServer(requestListener);
-server.listen(port, host, () => {
-  console.log(`Server is running on http://${host}:${port}`);
 });
+app.use(async (ctx, next) => {
+  await next();
+  if (fs.existsSync(publicPath + ctx.path)) {
+    ctx.set('Cache-Control', 'public');
+    await send(ctx, ctx.path, { root: publicPath });
+  }
+});
+console.log('test');
+const server = app.listen(process.env.PORT || 8080);
+module.exports = server;
