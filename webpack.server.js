a744abc067ad8ea48768c19c23e13e8c94c16a41
David Roffe <davidroffe@gmail.com>
2020-09-11 10:04:02 -0400

    Bundling svg the same way on server side as client to prevent react warning

--- webpack.server.js
+++ webpack.server.js
@@ -21,8 +21,8 @@ module.exports = {
         use: 'babel-loader',
       },
       {
-        test: /\.svg$/,
-        use: 'svg-url-loader',
+        test: /\.(jpe?g|png|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
+        loader: 'url-loader?limit=100000',
       },
     ],
   },
