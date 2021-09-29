80d457993adb01ecbd44a9a4bdef76602a397a9e
David Roffe <davidroffe@gmail.com>
2021-09-29 08:08:34 -0700

    Styled-component: Header

--- app/components/Header.js
+++ app/components/Header.js
@@ -3,6 +3,56 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Logo from '../assets/images/logo.svg';
+import styled from 'styled-components';
+
+const StyledHeader = styled.header`
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  padding: 0 50px;
+  @media (max-width: 720px) {
+    padding: 15px 45px 0;
+  }
+
+  .left {
+    display: flex;
+    @media (max-width: 720px) {
+      width: 100%;
+    }
+    .logo {
+      display: inline-block;
+      max-width: 150px;
+      margin-top: 15px;
+      @media (max-width: 720px) {
+        margin: 0;
+      }
+
+      img {
+        width: 100%;
+      }
+    }
+    #main-nav {
+      display: flex;
+      align-items: flex-end;
+      margin: 0 10px;
+
+      a {
+        margin: 0 8px 3px;
+        padding-bottom: 1px;
+        font-size: 1.2rem;
+        font-family: sans-serif;
+        font-weight: 600;
+        text-transform: uppercase;
+        color: #333;
+        border-bottom: 3px solid transparent;
+
+        &:hover {
+          border-bottom-color: #333;
+        }
+      }
+    }
+  }
+`;
 
 const mapStateToProps = (state) => {
   return {
@@ -13,7 +63,7 @@ const mapStateToProps = (state) => {
 
 const Header = (props) => {
   return (
-    <header id="main-header">
+    <StyledHeader id="main-header">
       <div className="left">
         <Link className="logo" to="/">
           <img src={Logo} alt="Imgpool Logo" />
@@ -28,7 +78,7 @@ const Header = (props) => {
         </nav>
       </div>
       {props.children}
-    </header>
+    </StyledHeader>
   );
 };
 
